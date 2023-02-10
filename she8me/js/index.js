b66b1a11985f6cb2d53cab0833b4d476588356c7
ssyuan88727 <116007315+ssyuan88727@users.noreply.github.com>
2023-02-11 00:35:42 +0800

    v02.11

---
+++ she8me/js/index.js
@@ -0,0 +1,15 @@
+$('#product_btn').click(function(){
+  $('#iframe').html('<iframe src="product_page.html" title="product" class="w-100 h-100 mt-3"></iframe>');
+});
+$('#supplier_btn').click(function(){
+  $('#iframe').html('<iframe src="supplier_page.html" title="supplier" class="w-100 h-100 mt-3"></iframe>');
+});
+$('#additional_btn').click(function(){
+  $('#iframe').html('<iframe src="additional_page.html" title="additional" class="w-100 h-100 mt-3"></iframe>');
+});
+$('#account_btn').click(function(){
+  $('#iframe').html('<iframe src="account_page.html" title="account" class="w-100 h-100 mt-3"></iframe>');
+});
+$('#document_btn').click(function(){
+  $('#iframe').html('<iframe src="document_page.html" title="document" class="w-100 h-100 mt-3"></iframe>');
+});
